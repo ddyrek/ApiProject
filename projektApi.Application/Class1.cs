0c85d1fd45ffdfb7f94b3190d1142e996506efde
ddyrek <51855332+ddyrek@users.noreply.github.com>
2023-01-19 21:35:07 +0100

    Add Application

---
+++ projektApi.Application/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace projektApi.Application
+{
+    //w  warstwie Aplikacji: logika, zapytania, kod do reagowania na każdy request
+    // tutaj przygotowujemy wszystkie dane które pózniej wysyłamy spowrotem do użytkownika do naszego klienta
+    //tu przygotowujemy wszystkie dane do zapisu
+    public class Class1
+    {
+
+    }
+}
---
+++ projektApi.Application/Common/Interfaces/IProjektApiDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using projektApi.Domain.Entities;
+
+namespace projektApi.Application.Common.Interfaces
+{
+    public interface IProjektApiDbContext
+    {
+        DbSet<Pies> Psy { get; set; }
+        DbSet<Kontrahent> Kontrahenci { get; set; }
+        DbSet<Klient> Klienci { get; set; }
+        DbSet<Wizyta> Wizyty { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
