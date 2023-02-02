878dcca20bf6d21db723c84643191c3f95dcbf64
ddyrek <51855332+ddyrek@users.noreply.github.com>
2023-02-02 22:03:54 +0100

    addCreateKlientCommandAndDeleteKlientCommand

---
+++ projektApi.Application/Klienci/Commands/DeleteKlient/DeleteKlientCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using projektApi.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projektApi.Application.Klienci.Commands.DeleteKlient
+{
+    public class DeleteKlientCommandHandler : IRequestHandler<DeleteKlientCommand>
+    {
+        private readonly IProjektApiDbContext _context;
+
+        public DeleteKlientCommandHandler(IProjektApiDbContext projektApiDbContext)
+        {
+            _context = projektApiDbContext;
+        }
+        public async Task<Unit> Handle(DeleteKlientCommand request, CancellationToken cancellationToken)
+        {
+            var director = await _context.Klienci.Where(p => p.Id == request.KlentId).FirstOrDefaultAsync(cancellationToken);
+
+            _context.Klienci.Remove(director);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+
+        }
+    }
+}
