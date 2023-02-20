63a836fee8fc52ddcb45aec8540f443b5829df8b
ddyrek <51855332+ddyrek@users.noreply.github.com>
2023-02-20 22:24:04 +0100

    ErrorInCreateCommandHandler

--- projektApi.Application/Kontrahenci/Commands/CreateKontrahent/CreateKontrahentCommandHandler.cs
+++ projektApi.Application/Kontrahenci/Commands/CreateKontrahent/CreateKontrahentCommandHandler.cs
@@ -56,7 +56,7 @@ namespace projektApi.Application.Kontrahenci.Commands.CreateKontrahent
             {
                 try
                 {
-                    await ValidRequest(request);
+                    //await ValidRequest(request);
                     var kontrahent = new Kontrahent();
                     _context.Kontrahenci.Add(kontrahent);
 
@@ -71,10 +71,10 @@ namespace projektApi.Application.Kontrahenci.Commands.CreateKontrahent
                 }
             }
 
-            private Task ValidRequest(CreateKontrahentCommand request)
-            {
-                if (string.IsNullOrEmpty(request.NazwaFirmy))
-                    throw new InvalidRequestException(request.GetType(), "NazwaFirmy", "Name is null or empty");
+            //private Task ValidRequest(CreateKontrahentCommand request)
+            //{
+                //if (string.IsNullOrEmpty(request.NazwaFirmy))
+                //    throw new InvalidRequestException(request.GetType(), "NazwaFirmy", "Name is null or empty");
                 //if (request.Name?.Length > 100)
                 //    throw new InvalidRequestException(request.GetType(), "Name", "Name is longer then 100 chars");
                 //if (request.Description == null)
@@ -112,7 +112,7 @@ namespace projektApi.Application.Kontrahenci.Commands.CreateKontrahent
                 //if (!request.Email.Contains('@'))
                 //    throw new InvalidRequestException(request.GetType(), "Email", "Email is not email");
 
-                return Task.CompletedTask;
-            }
+            //    return Task.CompletedTask;
+            //}
         }
 }
