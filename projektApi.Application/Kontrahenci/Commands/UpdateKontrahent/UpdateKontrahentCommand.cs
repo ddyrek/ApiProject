85f522f656be0816f3b749adf3c5cd7c03c8c81c
ddyrek <51855332+ddyrek@users.noreply.github.com>
2023-02-23 22:11:35 +0100

    AddUpdateKontrahentCommand

---
+++ projektApi.Application/Kontrahenci/Commands/UpdateKontrahent/UpdateKontrahentCommand.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MediatR;
+using projektApi.Application.Common.Mappings;
+using projektApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projektApi.Application.Kontrahenci.Commands.UpdateKontrahent
+{
+    public class UpdateKontrahentCommand : IRequest<int>, IMapFrom<UpdateKontrahentCommand>
+    {
+        public int KontrahentId { get; set; }
+        public string NazwaFirmy { get; set; }
+        public string Ulica { get; set; }
+        public string NumerBudynku { get; set; }
+        public string NumerLokalu { get; set; }
+        public string Nip { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            //utworzenie mapy z commanda na Entity.Kontrahenta
+            profile.CreateMap<UpdateKontrahentCommand, Kontrahent>()
+                .ForMember(x => x.NazwaFirmy, map => map.MapFrom(src => src.NazwaFirmy))
+                .ForMember(x => x.Ulica, map => map.MapFrom(src => src.Ulica))
+                .ForMember(x => x.NumerBudynku, map => map.MapFrom(src => src.NumerBudynku))
+                .ForMember(x => x.NumerLokalu, map => map.MapFrom(src => src.NumerLokalu))
+                .ForMember(x => x.Nip, map => map.MapFrom(src => src.Nip))
+                .IgnoreAllPropertiesWithAnInaccessibleSetter();
+            //.IgnoreAllNonExisting();
+        }
+    }
+}
