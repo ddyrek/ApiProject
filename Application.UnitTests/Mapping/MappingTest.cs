22cb2cd4d49d679732ff0800c1f781c08d33f9a1
ddyrek <51855332+ddyrek@users.noreply.github.com>
2023-03-16 23:12:38 +0100

    AddAplication.UnitTestAndInstalNugetDependenciesAndFirstTestConfigrationAutomapper

---
+++ Application.UnitTests/Mapping/MappingTest.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UnitTests.Mapping
+{
+    public class MappingTest : IClassFixture<MappingTestFixture>
+    {
+        private readonly IConfigurationProvider _configration;
+        private readonly IMapper _mapper;
+
+        public MappingTest(MappingTestFixture fixture)
+        {
+            _configration = fixture.ConfigurationProvider;
+            _mapper = fixture.Mapper;
+        }
+
+        //test konfiguracji automappera, Fact oznacza test
+        [Fact]
+        public void ShouldHaveValidConfiguration()
+        {
+            _configration.AssertConfigurationIsValid();
+        }
+    }
+}
