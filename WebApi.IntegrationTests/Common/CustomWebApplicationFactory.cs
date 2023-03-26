9e379f2d1593fa37f23c2a481894e277e7a3ea14
ddyrek <51855332+ddyrek@users.noreply.github.com>
2023-03-26 22:49:33 +0200

    Add-WebApi.IntegrationTests/Common/Utilities.cs

--- WebApi.IntegrationTests/Common/CustomWebApplicationFactory.cs
+++ WebApi.IntegrationTests/Common/CustomWebApplicationFactory.cs
@@ -1,94 +1,98 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
+using projektApi.Application.Common.Interfaces;
+using projektApi.Persistance;
 
 namespace WebApi.IntegrationTests.Common
 {
     public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
     {
-        //protected override void ConfigureWebHost(IWebHostBuilder builder)
-        //{
-        //    try
-        //    {
-        //        builder
-        //        .ConfigureServices(services =>
-        //        {
-        //            var serviceProvider = new ServiceCollection()
-        //                .AddEntityFrameworkInMemoryDatabase()
-        //                .BuildServiceProvider();
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            try
+            {
+                builder
+                .ConfigureServices(services =>
+                {
+                    var serviceProvider = new ServiceCollection()
+                        .AddEntityFrameworkInMemoryDatabase()
+                        .BuildServiceProvider();
 
-        //            services.AddDbContext<MovieDbContext>(options =>
-        //            {
-        //                options.UseInMemoryDatabase("InMemoryDbForTesting");
-        //                options.UseInternalServiceProvider(serviceProvider);
-        //            });
+                    services.AddDbContext<ProjektApiDbContext>(options =>
+                    {
+                        options.UseInMemoryDatabase("InMemoryDbForTesting");
+                        options.UseInternalServiceProvider(serviceProvider);
+                    });
 
-        //            services.AddScoped<IMovieDbContext>(provider => provider.GetService<MovieDbContext>());
-        //            services.AddScoped<ICurrentUserService, DummyCurrentUserService>();
-        //            var sp = services.BuildServiceProvider();
+                    services.AddScoped<IProjektApiDbContext>(provider => provider.GetService<ProjektApiDbContext>());
+                    services.AddScoped<ICurrentUserService, DummyCurrentUserService>();
+                    var sp = services.BuildServiceProvider();
 
-        //            using var scope = sp.CreateScope();
-        //            var scopedServices = scope.ServiceProvider;
-        //            var context = scopedServices.GetRequiredService<MovieDbContext>();
-        //            var logger = scopedServices.GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+                    using var scope = sp.CreateScope();
+                    var scopedServices = scope.ServiceProvider;
+                    var context = scopedServices.GetRequiredService<ProjektApiDbContext>();
+                    var logger = scopedServices.GetRequiredService<ILogger<CustomWebApplicationFactory<TProgram>>>();
 
-        //            context.Database.EnsureCreated();
+                    context.Database.EnsureCreated();
 
-        //            try
-        //            {
-        //                Utilities.InitilizeDbForTests(context);
-        //            }
-        //            catch (Exception ex)
-        //            {
-        //                logger.LogError(ex, "An error occurred seeding the " +
-        //                                    $"database with test messages. Error: {ex.Message}");
-        //            }
-        //        })
-        //        .UseSerilog()
-        //       .UseEnvironment("Test");
-        //    }
-        //    catch (Exception ex)
-        //    {
+                    try
+                    {
+                        Utilities.InitilizeDbForTests(context);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, "An error occurred seeding the " +
+                                            $"database with test messages. Error: {ex.Message}");
+                    }
+                })
+                .UseSerilog()
+               .UseEnvironment("Test");
+            }
+            catch (Exception ex)
+            {
 
-        //        throw;
-        //    }
+                throw;
+            }
 
-        //}
+        }
 
-        //public async Task<HttpClient> GetAuthenticatedClientAsync()
-        //{
-        //    var client = CreateClient();
+        public async Task<HttpClient> GetAuthenticatedClientAsync()
+        {
+            var client = CreateClient();
 
-        //    var token = await GetAccessTokenAsync(client, "alice", "Pass123$");
-        //    client.SetBearerToken(token);
-        //    return client;
-        //}
+            var token = await GetAccessTokenAsync(client, "alice", "Pass123$");
+            client.SetBearerToken(token);
+            return client;
+        }
 
-        //private async Task<string> GetAccessTokenAsync(HttpClient client, string userName, string password)
-        //{
-        //    var disco = await client.GetDiscoveryDocumentAsync();
+        private async Task<string> GetAccessTokenAsync(HttpClient client, string userName, string password)
+        {
+            var disco = await client.GetDiscoveryDocumentAsync();
 
-        //    if (disco.IsError)
-        //    {
-        //        throw new Exception(disco.Error);
-        //    }
+            if (disco.IsError)
+            {
+                throw new Exception(disco.Error);
+            }
 
-        //    var response = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
-        //    {
-        //        Address = disco.TokenEndpoint,
-        //        ClientId = "client",
-        //        ClientSecret = "secret",
-        //        Scope = "openid profile MovieManagement.ApiAPI api1",
-        //        UserName = userName,
-        //        Password = password
-        //    });
+            var response = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "openid profile MovieManagement.ApiAPI api1",
+                UserName = userName,
+                Password = password
+            });
 
-        //    if (response.IsError)
-        //    {
-        //        throw new Exception(response.Error);
-        //    }
+            if (response.IsError)
+            {
+                throw new Exception(response.Error);
+            }
 
-        //    return response.AccessToken;
-        //}
+            return response.AccessToken;
+        }
     }
 }
