316b6bbddeb8792b2d57977e39b7d16a3aabe96b
ddyrek <51855332+ddyrek@users.noreply.github.com>
2023-06-28 21:58:38 +0200

    AddKontrahentForm_blazorApiMaster_final

--- Groomer/Client/Models/KontrahentFormularz.cs
+++ Groomer/Client/Models/KontrahentFormularz.cs
@@ -1,11 +1,17 @@
-﻿namespace Groomer.Client.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Groomer.Client.Models
 {
     public class KontrahentFormularz
     {
+        [Required]
         public string? NazwaFirmy { get; set; }
+        [Required]
         public string? Ulica { get; set; }
+        [Required]
         public string? NumerBudynku { get; set; }
         public string? NumerLokalu { get; set; }
+        [Required]
         public string? Nip { get; set; }
     }
 }
