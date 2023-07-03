413f7e29b979c58552a80c8e67d04914b7858ce1
ddyrek <51855332+ddyrek@users.noreply.github.com>
2023-07-03 23:06:00 +0200

    AddApiLocal

---
+++ Groomer/Client/Models/KontrahentDetailVm.cs
@@ -0,0 +1,20 @@
+﻿namespace Groomer.Client.Models
+{
+    public class KontrahentDetailVm
+    {
+        public int Id { get; set; }
+        public string NazwaFirmy { get; set; }
+        public string Ulica { get; set; }
+        public string NumerBudynku { get; set; }
+        public string NumerLokalu { get; set; }
+        public string Nip { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime Created { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime Modified { get; set; }
+        public int StatusId { get; set; }
+        public string InactivatedBy { get; set; }
+        public DateTime Inactivated { get; set; }
+        //public string LastKlientName { get; set; }
+    }
+}
--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -68,7 +68,7 @@ public static class Config
                 RequireClientSecret = false,
                 
                 // scopes that client has access to
-                AllowedScopes = { "api1", "openid", "user", "role", "profile", IdentityServerConstants.LocalApi.ScopeName },
+                AllowedScopes = { "openid", "profile","user", "api1", IdentityServerConstants.LocalApi.ScopeName },
                 AllowedCorsOrigins = { "https://localhost:7088", "https://localhost:5001" },
                 RedirectUris = { "https://localhost:7088/authentication/login-callback" },
                 PostLogoutRedirectUris = { "https://localhost:7088/" },
