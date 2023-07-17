741a40b4c5ba730821c4094248d5802825ef63ac
ddyrek <51855332+ddyrek@users.noreply.github.com>
2023-07-17 23:31:51 +0200

    StartTemplateLteAdmin_add_css_and_scriptToIndex.htmlAndProramWrapper

--- Groomer/Client/Program.cs
+++ Groomer/Client/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<App>("#wrapper");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient("api")
