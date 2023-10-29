63f3b463ab1699afb13f62dd26b437302b7338d5
ddyrek <51855332+ddyrek@users.noreply.github.com>
2023-10-29 23:27:12 +0100

    Added_VisitAddDogs-NoOk

---
+++ Groomer/Shared/Dogs/Queries/AllDogsQuery/DogsForListVm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groomer.Shared.Dogs.Queries.AllDogsQuery
+{
+    public class DogsForListVm
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //public string Race { get; set; }
+        //public string? Description { get; set; }
+        //public int KlientId { get; set; }
+        //public int KontrahentId { get; set; }
+        //public Klient Klient { get; set; }
+        //public Kontrahent Kontrahent { get; set; }
+        //public ICollection<Wizyta> Wizyty
+    }
+}
