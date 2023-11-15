254a0e668c3ae4a7574d2c3f089b526d5550771a
WISNIOWSKI\d.dyrek <d.dyrek@wisniowski.pl>
2023-11-15 16:16:56 +0100

    AddVisistDetailVm.cs

---
+++ Groomer/Shared/Visits/Queries/DetailVisistQuery/VisistDetailVm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groomer.Shared.Visits.Queries.DetailVisistQuery
+{
+    public class VisistDetailVm
+    {
+        public int Id { get; set; }
+        public DateTime DataWizyty { get; set; }
+        public DateTime GodzinaWizyty { get; set; }
+        public string? Opis { get; set; }
+        public decimal? Kwota { get; set; }
+        public int PiesId { get; set; }
+        //public Pies Pies { get; set; }
+    }
+}
